3f50e8cfdb3c8e969f135f40ed5170fba2c476bd
Laurids Hieden <realjupi1001@gmail.com>
2023-10-22 13:21:06 +0200

    Actual working AI algorithm

--- script.js
+++ script.js
@@ -149,236 +149,84 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  document.getElementById("resetBoardButton").addEventListener("click", resetBoard);
+
+  function resetBoard() {
+    // Clear all squares
+    squares.forEach((square) => (square.innerHTML = "0"));
+
+    // Generate two new random tiles
+    generate();
+    generate();
+
+    // Reset the score
+    score = 0;
+    scoreDisplay.innerHTML = score;
+
+    // Reset the game over flag
+    gameOver = false;
+    won = false;
+  }
+
   document.getElementById("startAIButton").addEventListener("click", startAI);
 
   //------------------AI
   function startAI() {
-    if (gameOver || won) return;
-    let bestMove = findBestMove(squares, 3, -Infinity, Infinity, true);
-    console.log("Best move: " + bestMove.score + ", " + bestMove.move);
-    if (bestMove.move) {
-      let newBoard = makeMove(squares, bestMove.move, 2);
-      console.log(squares);
-      squares = newBoard;
+    let bestMove;
+    let bestScore = -Infinity;
 
-      console.log(squares);
+    // Simulate each possible move and evaluate the resulting board
+    for (let move of ["up", "down", "left", "right"]) {
+      let tempSquares = squares.slice(); // Create a copy of the current board
+      let tempScore = score; // Copy the current score
 
-      //generate();
-    }
-  }
-
-  function findBestMove(board, depth, alpha, beta, maximizingPlayer) {
-    if (depth === 0 || gameOver || won) {
-      return { score: evaluateBoard(board) };
-    }
-
-    if (maximizingPlayer) {
-      let maxEval = { score: -Infinity, move: null };
-
-      let moves = ["left", "up", "right", "down"];
-
-      for (let move of moves) {
-        let newBoard = makeMove([...board], move, 2);
-
-        if (!arraysEqual(newBoard, board)) {
-          let eval = findBestMove(newBoard, depth - 1, alpha, beta, false);
-
-          if (eval.score > maxEval.score) {
-            maxEval.score = eval.score;
-            maxEval.move = move;
-          }
-
-          alpha = Math.max(alpha, eval.score);
-
-          if (beta <= alpha) break;
-        }
+      if (move === "up") {
+        moveUp(tempSquares, tempScore);
+      } else if (move === "down") {
+        moveDown(tempSquares, tempScore);
+      } else if (move === "left") {
+        moveLeft(tempSquares, tempScore);
+      } else if (move === "right") {
+        moveRight(tempSquares, tempScore);
       }
 
-      return maxEval;
-    } else {
-      let minEval = { score: Infinity, move: null };
-      let emptyCells = getEmptyCells(board);
+      // Evaluate the resulting board after the simulated move
+      let evaluation = evaluateBoard(tempSquares, tempScore);
 
-      for (let cell of emptyCells) {
-        let newBoard2 = makeMove([...board], cell, 2);
-        let eval = findBestMove(newBoard2, depth - 1, alpha, beta, true);
-
-        if (eval.score < minEval.score) {
-          minEval.score = eval.score;
-          minEval.move = cell;
-        }
-
-        beta = Math.min(beta, eval.score);
-
-        if (beta <= alpha) break;
-      }
-
-      return minEval;
-    }
-  }
-
-  function makeMove(board, direction, value) {
-    let newBoard = [];
-    for (let i = 0; i < 4; i++) {
-      newBoard.push(board.slice(i * 4, i * 4 + 4));
-    }
-
-    switch (direction) {
-      case "left":
-        for (let i = 0; i < 4; i++) {
-          newBoard[i] = moveLeft(newBoard[i], value);
-        }
-        break;
-
-      case "up":
-        for (let i = 0; i < 4; i++) {
-          let col = [newBoard[0][i], newBoard[1][i], newBoard[2][i], newBoard[3][i]];
-          col = moveUp(col, value);
-          for (let j = 0; j < 4; j++) {
-            newBoard[j][i] = col[j];
-          }
-        }
-        break;
-
-      case "right":
-        for (let i = 0; i < 4; i++) {
-          newBoard[i] = moveRight(newBoard[i], value);
-        }
-        break;
-
-      case "down":
-        for (let i = 0; i < 4; i++) {
-          let col = [newBoard[0][i], newBoard[1][i], newBoard[2][i], newBoard[3][i]];
-          col = moveDown(col, value);
-          for (let j = 0; j < 4; j++) {
-            newBoard[j][i] = col[j];
-          }
-        }
-        break;
-
-      default:
-        break;
-    }
-
-    return [].concat(...newBoard);
-  }
-
-  function evaluateBoard(board) {
-    let emptyCells = getEmptyCells(board);
-    let smoothWeight = 0.1;
-    let monoWeight = 1.0;
-    let maxWeight = 1.0;
-
-    let smoothness = getSmoothness(board);
-    let monotonicity = getMonotonicity(board);
-    let maxTile = getMaxTile(board);
-
-    return smoothWeight * smoothness + monoWeight * monotonicity + maxWeight * maxTile;
-  }
-
-  function getEmptyCells(board) {
-    let emptyCells = [];
-    for (let i = 0; i < board.length; i++) {
-      if (board[i] === 0) {
-        emptyCells.push(i);
-      }
-    }
-    return emptyCells;
-  }
-
-  function arraysEqual(arr1, arr2) {
-    return JSON.stringify(arr1) === JSON.stringify(arr2);
-  }
-
-  function getSmoothness(board) {
-    let smoothness = 0;
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
-        if (board[i][j] !== 0) {
-          let value = Math.log2(board[i][j]);
-          let neighbors = getNeighbors(board, i, j);
-          for (let neighbor of neighbors) {
-            if (neighbor.value !== 0) {
-              let neighborValue = Math.log2(neighbor.value);
-              smoothness -= Math.abs(value - neighborValue);
-            }
-          }
-        }
-      }
-    }
-    return smoothness;
-  }
-
-  function getMonotonicity(board) {
-    let totals = [0, 0, 0, 0];
-
-    // Left and Right
-    for (let i = 0; i < 4; i++) {
-      let current = 0;
-      let next = current + 1;
-      while (next < 4) {
-        while (next < 4 && board[i][next] === 0) {
-          next++;
-        }
-        if (next >= 4) {
-          next--;
-        }
-        let currentValue = board[i][current] !== 0 ? Math.log2(board[i][current]) : 0;
-        let nextValue = board[i][next] !== 0 ? Math.log2(board[i][next]) : 0;
-        if (currentValue > nextValue) {
-          totals[0] += nextValue - currentValue;
-        } else if (nextValue > currentValue) {
-          totals[1] += currentValue - nextValue;
-        }
-        current = next;
-        next++;
+      // Keep track of the move that leads to the best evaluation
+      if (evaluation > bestScore) {
+        bestScore = evaluation;
+        bestMove = move;
       }
     }
 
-    // Up and Down
-    for (let i = 0; i < 4; i++) {
-      let current = 0;
-      let next = current + 1;
-      while (next < 4) {
-        while (next < 4 && board[next][i] === 0) {
-          next++;
-        }
-        if (next >= 4) {
-          next--;
-        }
-        let currentValue = board[current][i] !== 0 ? Math.log2(board[current][i]) : 0;
-        let nextValue = board[next][i] !== 0 ? Math.log2(board[next][i]) : 0;
-        if (currentValue > nextValue) {
-          totals[2] += nextValue - currentValue;
-        } else if (nextValue > currentValue) {
-          totals[3] += currentValue - nextValue;
-        }
-        current = next;
-        next++;
-      }
+    // Execute the best move
+    if (bestMove === "up") {
+      moveUp();
+    } else if (bestMove === "down") {
+      moveDown();
+    } else if (bestMove === "left") {
+      moveLeft();
+    } else if (bestMove === "right") {
+      moveRight();
     }
-
-    return Math.max(totals[0], totals[1]) + Math.max(totals[2], totals[3]);
   }
 
-  function getMaxTile(board) {
-    let maxTile = 0;
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
-        if (board[i][j] > maxTile) {
-          maxTile = board[i][j];
-        }
-      }
-    }
-    return Math.log2(maxTile);
-  }
+  function evaluateBoard(squares, score) {
+    let evaluation = 0;
 
-  function getNeighbors(board, i, j) {
-    let neighbors = [];
-    if (i > 0) neighbors.push({ row: i - 1, col: j, value: board[i - 1][j] });
-    if (i < 3) neighbors.push({ row: i + 1, col: j, value: board[i + 1][j] });
-    if (j > 0) neighbors.push({ row: i, col: j - 1, value: board[i][j - 1] });
-    if (j < 3) neighbors.push({ row: i, col: j + 1, value: board[i][j + 1] });
-    return neighbors;
+    // Reward for higher numbers
+    for (let square of squares) {
+      let value = parseInt(square.innerHTML);
+      evaluation += value * value;
+    }
+
+    // Reward for empty squares
+    evaluation += squares.filter((square) => square.innerHTML === "0").length;
+
+    // Reward for higher score
+    evaluation += score * 10;
+
+    return evaluation;
   }
 });
